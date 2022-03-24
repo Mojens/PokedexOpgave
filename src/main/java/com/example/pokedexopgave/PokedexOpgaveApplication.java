08a2b90457d9f71ab15293c6ef41ab463a16b14a
Mojens <moha135h@stud.kea.dk>
2022-03-24 10:35:02 +0100

    Initial commit

---
+++ src/main/java/com/example/pokedexopgave/PokedexOpgaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokedexopgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexOpgaveApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PokedexOpgaveApplication.class, args);
+  }
+
+}
